71e18ae60b0adde382dd135be9d74ab8bb65f6b3
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2024-05-01 19:51:14 +0530

    adds transaction controller, service, repository,dtos.

--- src/main/java/com/sm2k4/stocker/models/Transaction.java
+++ src/main/java/com/sm2k4/stocker/models/Transaction.java
@@ -1,5 +1,7 @@
 package com.sm2k4.stocker.models;
 
+import com.sm2k4.stocker.dtos.Transaction.TransactionRequestDTO;
+import com.sm2k4.stocker.dtos.Transaction.TransactionResponseDTO;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -13,17 +15,29 @@ public class Transaction {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @ManyToOne
     private Stock stockId;
-
     @ManyToOne
     private Trader traderId;
-
     @Column(nullable = false)
     private Long qty;
+
+    private TransactionStatus status;
+
     @Column(nullable = false)
     private Type type;
     @Column(nullable = false)
     private Date date;
+
+    public TransactionResponseDTO mapToTransactionResponse() {
+        TransactionResponseDTO transactionResponseDTO = new TransactionResponseDTO();
+        transactionResponseDTO.setId(id);
+        transactionResponseDTO.setStockId(stockId);
+        transactionResponseDTO.setTraderId(traderId);
+        transactionResponseDTO.setQty(qty);
+        transactionResponseDTO.setStatus(status);
+        transactionResponseDTO.setType(type);
+        transactionResponseDTO.setDate(date);
+        return transactionResponseDTO;
+    }
 }
